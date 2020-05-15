a265fbc1ed0c55c36d7cc565b155163efad7c569
性空山 <38686208+fairymeee@users.noreply.github.com>
2020-05-15 11:35:30 +0800

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const {CookieAccessInfo, CookieJar, Cookie} = cookiejar
 
 let config = {
     httpprefix: 'https', port: 443,
-    serverName: 'siteproxy.netptop.com',
+    serverName: 'fairymeee.now.sh',
 }
 let blockedSites = ['merlinblog.xyz']
 
